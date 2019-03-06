29f63e1b9a8fc1448c6bc1ca96dfbf727e23eb7e
stefanfortuin <stefanfortuin02@gmail.com>
2019-03-06 14:01:38 +0100

    options for adding stuff

--- src/components/ButtonCreate.vue
+++ src/components/ButtonCreate.vue
@@ -1,16 +1,32 @@
 <template>
-	<div class="button-create" @click="create">
-		<img src="@/assets/plus.svg"/>
-	</div>
+  <div>
+    <div class="button-create" @click="show = !show">
+      <img v-if="!show" class="image" src="@/assets/plus.svg">
+      <img v-else class="image" src="@/assets/arrow.svg">
+    </div>
+	<transition name="slide-right">
+    	<create-options v-if="show" @close="show = !show"></create-options>
+	</transition>
+  </div>
 </template>
 
 <script>
+import CreateOptions from "@/components/CreateOptions";
+
 export default {
-	name: "button-create",
-	methods: {
-		create(){
-			this.$router.push({path: '/create'})
-		}
-	}
-}
+  name: "button-create",
+  components: {
+    CreateOptions
+  },
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    create() {
+      this.$router.push({ path: "/create" });
+    }
+  }
+};
 </script>
---
+++ src/components/CreateOptions.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="create-options">
+		<div class="options" v-if="!creating">
+			<div class="option-button" @click="create">Cake</div>
+			<div class="option-button">Cupcake</div>
+		</div>
+		<div class="options" v-else>
+			<div class="option-button" @click="layer">Layer</div>
+			<div class="option-button">Decoration</div>
+			<div class="option-button">Topper</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "create-options",
+	methods:{
+		create(){
+			this.$router.push({path: '/create'});
+			this.close();
+		},
+		layer(){
+			this.$root.$emit('addlayer');
+			this.close();
+		},
+		close(){
+			this.$emit('close');
+		}
+	},
+	computed: {
+		creating(){
+			return this.$route.path == '/create' ? true : false;
+		}
+	}
+}
+</script>
