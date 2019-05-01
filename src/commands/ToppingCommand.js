4ee9fabff929a90127243e62f53b2cfb91348779
Stefan Fortuin <stefanfortuin02@gmail.com>
2019-05-01 17:03:19 +0200

    topping changes

--- src/commands/ToppingCommand.js
+++ src/commands/ToppingCommand.js
@@ -1,18 +1,22 @@
 import Cake from "../models/Cake";
 import store from '../store';
+import Topping from "../models/Topping";
 
 export default class ToppingCommand{
 	constructor(type){
-		this.type = type;
-		this.cake = store.getters.getScene.find(Cake);
+		this._type = type;
+		this._cake = store.getters.getScene.find(Cake);
+		this._height = this._cake.Height();
+		this._topLayer = this._cake.TopLayer();
+		this._topping = new Topping(this._height, this._topLayer.scale.x - 0.1);
 	}
 
 	Execute(){
-		this.cake.Topping();
+		this._cake.add(this._topping);
 	}
 
 	Undo(){
-		this.cake.DeleteTopping();
+		this._cake.remove(this._topping);
 	}
 
 	Redo(){
