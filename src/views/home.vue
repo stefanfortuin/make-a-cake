24cd08878d01df69dc65e1c02f714769ad2a67c4
Stefan Fortuin <stefanfortuin02@gmail.com>
2019-05-01 17:41:03 +0200

    some home page changes, main button attract animation

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,41 @@
 <template>
 	<div class="home">
-
+		<div class="title">Create your own cake!</div>
+		<div class="info">Be creative and create something beautiful</div>
+		<div class="features">
+			<div class="feature">
+				<div class="feature-img">
+					<img src="@/assets/option_color.svg">
+				</div>
+				<div class="feature-info">
+					Color the parts
+				</div>
+			</div>
+			<div class="feature">
+				<div class="feature-img">
+					<img src="@/assets/option_size.svg">
+				</div>
+				<div class="feature-info">
+					Resize the parts
+				</div>
+			</div>
+			<div class="feature">
+				<div class="feature-img">
+					<img src="@/assets/option_shape.svg">
+				</div>
+				<div class="feature-info">
+					Different shapes
+				</div>
+			</div>
+			<div class="feature">
+				<div class="feature-img">
+					<img src="@/assets/option_rotate.svg">
+				</div>
+				<div class="feature-info">
+					Rotate the parts
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
